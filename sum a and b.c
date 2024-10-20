607898550686c10c55ab3c68c7d0f3d8f1268e60
I-abMoeez <imoeezqureshi@gmail.com>
2024-10-20 12:45:15 +0500

    Add files via upload

---
+++ sum a and b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+    int a;
+    int b;
+    
+    printf("entre the value of a");
+    scanf("%d",&a);
+    printf("entre the value of b");
+    scanf("%d",&b);
+
+    int sum=a+b;
+   printf("sum of a and b is %d",sum);
+   return 0;
+   
+}
