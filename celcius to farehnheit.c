7252e95423e8a5fc1eab2a422fe3a4cae58470d3
I-abMoeez <imoeezqureshi@gmail.com>
2024-10-20 12:53:21 +0500

    Add files via upload

---
+++ celcius to farehnheit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int celcius;
+    printf("celcius to fahrenheit");
+    scanf("%d",&celcius);
+    float fr=(celcius *9.0/5.0)+32;
+    printf("value of fr is %.2f",fr);
+    getc;
+    return 0;
+    
+}
