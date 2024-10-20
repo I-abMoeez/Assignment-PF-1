f07884bf54c9675a26f775f452a08dce9a874991
I-abMoeez <imoeezqureshi@gmail.com>
2024-10-20 12:47:29 +0500

    Add files via upload

---
+++ square.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("square of any value of a");
+    scanf("%d",&a);
+    int square=a*a;
+    printf("square of %d",square );
+    return 0;
+
+}
